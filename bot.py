f58e2317b43aaa8f930a9390ab4fbe516f2b3fa7
Xyntexx <mnuuja@gmail.com>
2019-11-03 18:36:04 +0200

    backup

--- bot.py
+++ bot.py
@@ -4,6 +4,7 @@
 
 import logging, time, sys
 from data import Data
+from telegram import Bot  
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from w1thermsensor import W1ThermSensor
 
@@ -17,6 +18,7 @@ sensor = W1ThermSensor()
 extemp = Data("extemp", sensor.get_temperature)
 
 
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
@@ -24,17 +26,62 @@ def start(update, context):
     update.message.reply_text('Hi!')
 
 def now(update, context):
-    update.message.reply_text("{:.1f} °C \n{:.1f} °C/min \n{:.1f} °C/h"
+    update.message.reply_text("{:.1f} °C \n{:.2f} °C/min \n{:.1f} °C/h"
     .format(extemp.now, extemp.delta_short(), extemp.delta_long()))
 
 def help(update, context):
     """Send a message when the command /help is issued."""
     update.message.reply_text('Help!')
 
+alert = False
+
+def check_alert(bot):
+    global alert
+    lowtemp = extemp.now < 45
+    if lowtemp != alert:
+        if lowtemp:
+            alert = True
+            for i in subscribers:
+                bot.bot.send_message(chat_id=i, text="lämpötilahälytys!!")
+        else:
+            alert = False
+
+def update_subs():
+    with open('subs.txt', 'w') as the_file:
+        for i in subscribers:
+            the_file.write(str(i)+"\n")
+
+def read_subs():
+    subs=[]
+    try:
+        with open('subs.txt', 'r') as the_file:
+            for i in the_file:
+                subs.append(int(i))
+    except FileNotFoundError:
+        open('subs.txt', 'a').close()
+    return subs
+
+subscribers = read_subs()
 
 def echo(update, context):
     """Echo the user message."""
     update.message.reply_text(update.message.text)
+
+def subscribe(update, context):
+    id = update.effective_message.chat_id
+    if(id in subscribers):
+        update.message.reply_text("Tilaus jo olemassa")
+    else:
+        subscribers.append(id)
+        update.message.reply_text("Tilaus lisätty")
+
+def unsubscribe(update, context):
+    id = update.effective_message.chat_id
+    if(id in subscribers):
+        subscribers.remove(id)
+        update.message.reply_text("Tilaus poistettu")
+    else:
+        update.message.reply_text("Ei tilausta")
 
 
 def error(update, context):
@@ -46,7 +93,9 @@ def read_token():
     return file.readline()
 
 def main():
+
     """Start the bot."""
+
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
@@ -60,6 +109,12 @@ def main():
     dp.add_handler(CommandHandler("help", help))
     dp.add_handler(CommandHandler("nyt", now))
     dp.add_handler(CommandHandler("now", now))
+    dp.add_handler(CommandHandler("subscribe", subscribe))
+    dp.add_handler(CommandHandler("tilaa", subscribe))
+    dp.add_handler(CommandHandler("poista", unsubscribe))
+    dp.add_handler(CommandHandler("remove", unsubscribe))
+    dp.add_handler(CommandHandler("unsubscribe", unsubscribe))
+    dp.add_handler(CommandHandler("stop", unsubscribe))
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
 
@@ -68,7 +123,6 @@ def main():
 
     # Start the Bot
     updater.start_polling()
-
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
@@ -77,11 +131,16 @@ def main():
     try:
         while(True):
             extemp.update()
+            check_alert(updater)
             time.sleep(60)
+            if time.localtime().tm_min == 0:
+                update_subs()
+                extemp.save()
     except (KeyboardInterrupt, SystemExit):
         print("Saving and quitting")
-        extemp.dumpall()
+        extemp.save()
         updater.stop()
+        update_subs()
     except:
         print("Unexpected error:", sys.exc_info()[0])
         raise
--- data.py
+++ data.py
@@ -1,6 +1,6 @@
 import time
 from tinydb import TinyDB, Query
-TAIKANUMERO = 60
+TAIKANUMERO = 10080 #astetta
 
 class Data:
     def __init__(self,name, pointer):
@@ -9,36 +9,39 @@ class Data:
         self.command = pointer
         self.now = 0.0
         self.minute=[]
-    
+        self.new = 0
+        for i in self.db:
+            self.minute.append((i["time"],i["value"]))
+
     def update(self):
+        self.new += 1
         self.now = self.command()
-        self.minute.insert(0, (time.strftime("%Y-%m-%d %H:%M"),self.now))
+        self.minute.append((time.strftime("%Y-%m-%d %H:%M"),self.now))
 
     def delta_short(self):
         length =len(self.minute)
         if length >= 5:
-            delta = self.minute[0][1]-self.minute[4][1]
+            delta = self.minute[-1][1]-self.minute[-5][1]
             return delta/5
         else:
-            delta = self.minute[0][1]-self.minute[length-1][1]
+            delta = self.minute[-1][1]-self.minute[0][1]
             return delta/length
 
 
     def delta_long(self):
         length =len(self.minute)
         if length >= 60:
-            delta = self.minute[0][1]-self.minute[60][1]
+            delta = self.minute[-1][1]-self.minute[-60][1]
             return delta*60/60
         else:
-            delta = self.minute[0][1]-self.minute[length-1][1]
+            delta = self.minute[-1][1]-self.minute[0][1]
             return delta*60/length
 
     def save(self):
-        while (len(self.minute))>TAIKANUMERO:
-            last = self.minute.pop()
-            self.db.insert({'time': last[0], 'value': last[1]})
-            
-    def dumpall(self):
-        while (self.minute):
-            last = self.minute.pop()
-            self.db.insert({'time': last[0], 'value': last[1]})
+        while self.new:
+            self.db.insert({'time': self.minute[-self.new][0], 'value': self.minute[-self.new][1]})
+            self.new -= 1
+    
+    def day()
+
+    def week()
